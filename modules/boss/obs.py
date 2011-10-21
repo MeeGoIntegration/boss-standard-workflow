5ad0192255dcfaa129d4db69cc0fe0311948286e
Pami Ketolainen <ext-pami.o.ketolainen@nokia.com>
2011-10-21 16:46:27 +0300

    Fixes in boss.obs module

--- modules/boss/obs.py
+++ modules/boss/obs.py
@@ -1,58 +1,96 @@
 """Helper classes for participants which deal with OBS."""
+from functools import wraps
 from urllib2 import HTTPError
 from buildservice import BuildService
+
 
 class OBSError(Exception):
     """Exception identifying OBS errors."""
     pass
 
-class BuildServiceMixin(object):
+class BuildServiceParticipant(object):
     """Participant Mixin class for general BuildService setup."""
 
     def __init__(self, *args, **kwargs):
         # pylint: disable=W0613
+        self.__obs_mocked = False
         self.__obs = None
         self.__obs_alias = None
         self.__oscrc = None
 
-    def handle_lifecycle_control(self, ctrl):
-        """Get OBS config"""
-        if ctrl.message == "start":
-            if ctrl.config.has_option("obs", "oscrc"):
-                self.__oscrc = ctrl.config.get("obs", "oscrc")
-            else:
-                raise RuntimeError("Missing configuration value: [obs] oscrc")
+    @classmethod
+    def get_oscrc(cls, method):
+        """Decorator for getting [obs] oscrc from participant configuration.
 
-    def handle_wi(self, wid):
-        """Get the namespace from workitem"""
-        if not (wid.fields.ev and wid.fields.ev.namespace):
-            raise RuntimeError("Mandatory field ev.namespace missing")
-        if wid.fields.ev.namespace != self.__obs_alias:
-            self.__obs_alias = wid.fields.ev.namespace
-            self.__obs = None
+        Can be used on participant handle_lifecycle_control() method.
+        """
+        @wraps(method)
+        def wrapper(self, ctrl):
+            # pylint: disable=C0111
+            if not isinstance(self, cls):
+                raise RuntimeError("get_oscrc decoarator can only be used for "
+                    "methods of %s subclasses" % cls.__name__)
+
+            if ctrl.message == "start":
+                if ctrl.config.has_option("obs", "oscrc"):
+                    self.__oscrc = ctrl.config.get("obs", "oscrc")
+                else:
+                    raise RuntimeError("Missing configuration value: "
+                            "[obs] oscrc")
+            return method(self, ctrl)
+        return wrapper
+
+    @classmethod
+    def setup_obs(cls, method):
+        """Decorator to get the namespace from workitem.
+
+        Can be used on participant handle_wi() method.
+        """
+        @wraps(method)
+        def wrapper(self, wid):
+            # pylint: disable=C0111
+            if not isinstance(self, cls):
+                raise RuntimeError("setup_obs decoarator can only be used for "
+                    "methods of %s subclasses" % cls.__name__)
+
+            if not (wid.fields.ev and wid.fields.ev.namespace):
+                raise RuntimeError("Mandatory field ev.namespace missing")
+            if wid.fields.ev.namespace != self.__obs_alias:
+                self.__obs_alias = wid.fields.ev.namespace
+                if not self.__obs_mocked:
+                    self.__obs = None
+            return method(self, wid)
+        return wrapper
 
     @property
     def obs(self):
         """Lazy BuildService property."""
         if self.__obs is None:
+            if self.__oscrc is None or self.__obs_alias is None:
+                raise RuntimeError("BuildService conf values not set. "
+                        "Use get_oscrc and setup_obs decorators.")
             self.__obs = BuildService(
                     oscrc=self.__oscrc,
                     apiurl=self.__obs_alias)
         return self.__obs
 
+    @obs.setter
+    def obs(self, instance):
+        """Setter needed for mocking BuildService in unit tests."""
+        if instance.__class__.__name__ != "Mock":
+            raise AttributeError("obs is read only property")
+        self.__obs_mocked = True
+        self.__obs = instance
+
 
 class RepositoryMixin(object):
     """Participant Mixin class for resolving repositories."""
-    def __init__(self, *args, **kwargs):
-        # pylint: disable=W0613
-        if not isinstance(self, BuildServiceMixin):
-            raise RuntimeError("RepositoryMixin requires BuildServiceMixin")
 
-    def __get_repositories(self, wid, action, prefix):
+    def __get_repositories(self, wid, action, project_id):
         """Generic method to fetch either source or target repos."""
-        project = action.get(prefix + "project", None)
+        project = action.get(project_id, None)
         if not project:
-            return None
+            raise RuntimeError("%s not defined in action" % project_id)
         # Check if repos already in workitem
         if wid.fields.repositories is not None:
             result = getattr(wid.fields.repositories, project, None)
@@ -80,6 +118,7 @@ class RepositoryMixin(object):
                 raise OBSError("getRepositoryArchs(%s, %s) failed: %s" %
                     (project, repo, exobj))
         setattr(wid.fields.repositories, project, result)
+        return result
 
     def get_target_repos(self, wid, action):
         """Get target repositories for action.
@@ -91,7 +130,7 @@ class RepositoryMixin(object):
         Retruns dictionary of repositories with list of architectures for the
         target project of given action.
         """
-        return self.__get_repositories(wid, action, "target")
+        return self.__get_repositories(wid, action, "targetproject")
 
     def get_source_repos(self, wid, action):
         """Get source repositories for action.
@@ -103,4 +142,4 @@ class RepositoryMixin(object):
         Retruns dictionary of repositories with list of architectures for the
         source project of given action.
         """
-        return self.__get_repositories(wid, action, "source")
+        return self.__get_repositories(wid, action, "sourceproject")
