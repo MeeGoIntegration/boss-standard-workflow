fc517e45ff966dfb58e8fb4475d2be7291b61288
Pami Ketolainen <ext-pami.o.ketolainen@nokia.com>
2011-10-25 14:40:33 +0300

    Modifications to boss.obs module
    * Changed the repository information format in RepositoryMixin
    * Improved documentation

--- modules/boss/obs.py
+++ modules/boss/obs.py
@@ -9,7 +9,30 @@ class OBSError(Exception):
     pass
 
 class BuildServiceParticipant(object):
-    """Participant Mixin class for general BuildService setup."""
+    """Base class for participants using BuildService.
+
+    Automates the BuildService setup with decorators for participant
+    handle_lifecycle_control() and handle_wi().
+
+    Example::
+
+        class ParticipantHandler(BuildServiceParticipant):
+
+            @BuildServiceParticipant.get_oscrc
+            def handle_lifecycle_control(self, ctrl):
+                # Decorator gets the required osc config from control message at
+                # start
+                pass
+
+            @BuildServiceParticipant.setup_obs
+            def handle_wi(self, wid):
+                # Decorator instantiates the BbuildService based on ev.namespace
+                # field provided in workitem. BuildService is available as
+                # property self.obs
+
+                ...
+
+    """
 
     def __init__(self, *args, **kwargs):
         # pylint: disable=W0613
@@ -48,7 +71,7 @@ class BuildServiceParticipant(object):
         """
         @wraps(method)
         def wrapper(self, wid):
-            # pylint: disable=C0111
+            # pylint: disable=C0111,W0212
             if not isinstance(self, cls):
                 raise RuntimeError("setup_obs decoarator can only be used for "
                     "methods of %s subclasses" % cls.__name__)
@@ -74,8 +97,8 @@ class BuildServiceParticipant(object):
                     apiurl=self.__obs_alias)
         return self.__obs
 
-    @obs.setter
-    def obs(self, instance):
+    @obs.setter # pylint: disable=E1101
+    def __set_obs(self, instance):
         """Setter needed for mocking BuildService in unit tests."""
         if instance.__class__.__name__ != "Mock":
             raise AttributeError("obs is read only property")
@@ -84,13 +107,53 @@ class BuildServiceParticipant(object):
 
 
 class RepositoryMixin(object):
-    """Participant Mixin class for resolving repositories."""
+    """Participant Mixin class for fetching repository information from OBS.
 
-    def __get_repositories(self, wid, action, project_id):
-        """Generic method to fetch either source or target repos."""
-        project = action.get(project_id, None)
-        if not project:
-            raise RuntimeError("%s not defined in action" % project_id)
+    Provided methods require BuildService as member {{{obs}}}, so using
+    BuildServiceParticipant is recommended.
+
+    Example::
+
+        class ParticipantHandler(BuildServiceParticipant, RepositoryMixin):
+
+            @BuildServiceParticipant.get_oscrc
+            def handle_lifecycle_control(self, ctrl):
+                pass
+
+            @BuildServiceParticipant.setup_obs
+            def handle_wi(self, wid):
+                repos = self.get_project_repos("someproject")
+                # Do something with the repository info
+
+                ...
+
+    """
+
+    def get_project_repos(self, project, wid=None):
+        """Get project repository information.
+
+        :param project: Project name
+        :param wid: Workitem used to cache repository info
+            * If None, no caching is used
+        :returns: Dictionary containing the repository information
+
+        If workitem is not given or it does not contain the repository
+        information for requested project, then the information is fetched from
+        OBS.
+
+        Returned dictionary has the following format::
+
+            {
+                "repository_name": {
+                    "path": path of this repository,
+                    "targets": paths of the repositories this repository builds
+                               against
+                    "architectures": list of architectures for this repository
+                    },
+                ...
+            }
+
+        """
         # Check if repos already in workitem
         if wid.fields.repositories is not None:
             result = getattr(wid.fields.repositories, project, None)
@@ -112,34 +175,44 @@ class RepositoryMixin(object):
                     (project, msg))
 
         for repo in repositories:
+            result[repo] = {"path": "%s/%s" % (project, repo)}
             try:
-                result[repo] = self.obs.getRepositoryArchs(project, repo)
+                result[repo]["architectures"] = self.obs.getRepositoryArchs(
+                        project, repo)
             except HTTPError, exobj:
                 raise OBSError("getRepositoryArchs(%s, %s) failed: %s" %
+                    (project, repo, exobj))
+            try:
+                result[repo]["targets"] = self.obs.getRepositoryTargets(
+                        project, repo)
+            except HTTPError, exobj:
+                raise OBSError("getRepositoryTargets(%s, %s) failed: %s" %
                     (project, repo, exobj))
         setattr(wid.fields.repositories, project, result)
         return result
 
-    def get_target_repos(self, wid, action):
-        """Get target repositories for action.
+    def get_target_repos(self, action, wid=None):
+        """Get target project repositories for action.
 
-        :param wid: Workitem
         :param action: OBS request action dictionary
-        :retruns: {"repositoryname": ["architecture",...], ...}
-
-        Retruns dictionary of repositories with list of architectures for the
-        target project of given action.
+        :param wid: Workitem used to cache repository info
+            * If None, no caching is used
+        :retruns: Repository info with same format as get_project_repos()
         """
-        return self.__get_repositories(wid, action, "targetproject")
+        project = action.get("targetproject", None)
+        if not project:
+            raise RuntimeError("targetproject not defined in action")
+        return self.get_project_repos(project, wid)
 
-    def get_source_repos(self, wid, action):
+    def get_source_repos(self, action, wid=None):
         """Get source repositories for action.
 
-        :param wid: Workitem
         :param action: OBS request action dictionary
-        :retruns: {"repositoryname": ["architecture",...], ...}
-
-        Retruns dictionary of repositories with list of architectures for the
-        source project of given action.
+        :param wid: Workitem used to cache repository info
+            * If None, no caching is used
+        :retruns: Repository info with same format as get_project_repos()
         """
-        return self.__get_repositories(wid, action, "sourceproject")
+        project = action.get("sourceproject", None)
+        if not project:
+            raise RuntimeError("sourceproject not defined in action")
+        return self.get_project_repos(project, wid)
