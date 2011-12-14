d8c1921bf107196a3c906ccea3ed6d6b29718f81
Pami Ketolainen <ext-pami.o.ketolainen@nokia.com>
2011-12-14 12:37:51 +0200

    Use get_provides results in get_kickstarts

--- participants/get_kickstarts.py
+++ participants/get_kickstarts.py
@@ -8,19 +8,19 @@ files from them to workitem images list.
 :term:`Workitem` fields IN
 
 :Parameters:
-    project:
-        The configuration package is looked from this projects
-    ev.actions(List):
-        (Optional) If this is SR, look for the conf package in actions.
+    ev.namespace(string):
+        Namespace to use, see here:
+        http://wiki.meego.com/Release_Infrastructure/BOSS/OBS_Event_List
+    image_configurations(dictionary):
+        Dictionary of image configuration providing binaries as returned by
+        get_provides participant.
     ignore_ks:
         (Optional) List of kickstart file name patterns to ignore
 
 
-:term:`Workitem` parameters IN
-
-:Parameters:
-    conf_package:
-        OBS name of the configuration package
+:term:'Workitem' params IN:
+    project(string):
+        Project to download kickstarts binaries from
 
 
 :term:`Workitem` fields OUT
@@ -36,7 +36,7 @@ files from them to workitem images list.
        True if kickstart(s) were found, false otherwise
 
 """
-import os, re, shutil
+import os, re
 
 from boss.obs import BuildServiceParticipant, RepositoryMixin
 from boss.lab import Lab
@@ -45,10 +45,6 @@ from boss.rpm import extract_rpm
 
 class ParticipantHandler(BuildServiceParticipant, RepositoryMixin):
     """Participant class as defined by the SkyNET API."""
-
-    def __init__(self):
-        self.storage_path = None
-        super(ParticipantHandler, self).__init__()
 
     def handle_wi_control(self, ctrl):
         """Job control thread."""
@@ -63,54 +59,49 @@ class ParticipantHandler(BuildServiceParticipant, RepositoryMixin):
     def handle_wi(self, wid):
         """Job thread."""
         wid.result = False
-        if not wid.params.conf_package:
-            raise RuntimeError("Mandatory parameter 'conf_package' missing")
-        if not wid.fields.project:
-            raise RuntimeError("Mandatory field 'project' missing")
-        project = wid.fields.project
-        package = wid.params.conf_package
+        if not wid.fields.image_configurations:
+            raise RuntimeError("Mandatory field 'image_configurations' missing")
+        if not wid.params.project:
+            raise RuntimeError("Mandatory parameter 'project' missing")
+
         if wid.fields.ignore_ks is not None and \
                 not isinstance(wid.fields.ignore_ks, list):
             raise RuntimeError("Field 'ignore_ks' has to be a list")
+
         ignore = [re.compile(pat) for pat in wid.fields.ignore_ks or []]
-
-        # Find the package
-        # If configuration package is in submit request sources, get it from
-        # that project
-        if wid.fields.ev and wid.fields.ev.actions:
-            for action in wid.fields.ev.actions:
-                if action.get("sourcepackage", None) == package:
-                    project = action["sourceproject"]
-        targets = self.get_project_targets(project, wid=wid)
-
-        images = []
-        with Lab(prefix="ks_downloader") as lab:
-            for fname in self._download_kickstarts(lab.path, project, package,
-                    targets):
-                ks_name = os.path.basename(fname)
-                if [True for pattern in ignore if pattern.match(ks_name)]:
-                    continue
-                images.append({
-                    "name": os.path.splitext(ks_name)[0],
-                    "kickstart":lab.open(fname).read()})
+        images = self._download_kickstarts(wid.params.project,
+                wid.fields.image_configurations, ignore)
 
         wid.fields.images = images
         if images:
             wid.result = True
 
 
-    def _download_kickstarts(self, target_dir, project, package, targets):
-        """Downloads RPMs for given package."""
-        rpm_files = []
-        for target in targets:
-            for binary in self.get_binary_list(project, package, target):
-                if not binary.endswith(".rpm") or binary.endswith(".src.rpm")\
-                        or binary in rpm_files:
-                    continue
-                rpm_files.append(binary)
-                self.download_binary(project, package, target, binary,
-                        target_dir)
+    def _download_kickstarts(self, project, configurations, ignore):
+        """Downloads RPM and extrack .ks files."""
+        images = []
+        rpms = []
         ks_files = set()
-        for rpm in rpm_files:
-            ks_files.update(extract_rpm(rpm, target_dir, patterns=["*.ks"]))
-        return ks_files
+        with Lab(prefix="ks_downloader") as lab:
+            # Download binaries
+            for package in configurations:
+                for target in configurations[package]:
+                    for binary in configurations[package][target]:
+                        rpms.append(self.download_binary(project, package,
+                                target, binary, lab.path))
+
+            # Extract kickstart files
+            for rpm in rpms:
+                ks_files.update(extract_rpm(rpm, lab.path, patterns=["*.ks"]))
+            
+            # Read ks contents in images array
+            for fname in ks_files:
+                # TODO: normalize names to prevent collisions
+                ks_name = os.path.basename(fname)
+                if [True for pattern in ignore if pattern.match(ks_name)]:
+                    continue
+                images.append({
+                    "name": os.path.splitext(ks_name)[0],
+                    "kickstart": lab.open(fname).read()})
+
+        return images
