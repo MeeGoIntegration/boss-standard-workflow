bbbd58fb43627b0ea6fb1e957de04e5ab896046f
Richard Braakman <ext-richard.braakman@nokia.com>
2011-12-30 14:06:03 +0200

    [bz] Extend unit test coverage

--- tests/test_bz.py
+++ tests/test_bz.py
@@ -1,6 +1,7 @@
 import unittest
 from mock import Mock
-from ConfigParser import SafeConfigParser
+import ConfigParser
+import Cheetah
 
 from bz import prepare_comment
 from common_test_lib import BaseTestParticipantHandler
@@ -14,6 +15,10 @@ class TestBzFunctions(unittest.TestCase):
     def test_prepare_comment_utf8(self):
         text = prepare_comment("testtemplate $key", {"key": u"\xe1\xe1"})
         self.assertEqual(text, u"testtemplate \xe1\xe1".encode('utf-8'))
+
+    def test_prepare_comment_notfound(self):
+        self.assertRaises(Cheetah.Template.NotFound,
+            prepare_comment, "testtemplate $missingkey", {"key": "value"})
 
 class TestParticipantHandler(BaseTestParticipantHandler):
 
@@ -30,16 +35,27 @@ class TestParticipantHandler(BaseTestParticipantHandler):
         self.mut.BugzillaREST = Mock()
         self.mut.BugzillaREST.side_effect = self.mockzilla
 
-        self.config = SafeConfigParser()
+        self.config = ConfigParser.SafeConfigParser()
         self.config.read("tests/test_data/bugzilla_right.conf")
 
         self.bugnum = "1234"
         self.changelog = "* Wed Aug 10 2011 Dmitry Rozhkov <dmitry@example.com> - 0.6.1\n- made changes fixing BMC#%s" % self.bugnum
 
     def test_setup_config_bad(self):
-        config = SafeConfigParser()
-        config.read("tests/test_data/bugzilla_wrong.conf")
-        self.assertRaises(Exception, self.participant.setup_config, config)
+        self.config.remove_option('meego', 'method')
+        self.assertRaises(ConfigParser.NoOptionError,
+            self.participant.setup_config, self.config)
+
+    def test_setup_config_bad_filename(self):
+        self.config.set('meego', 'comment_template',
+                        'tests/test_data/non/existing/filename')
+        self.assertRaises(RuntimeError,
+            self.participant.setup_config, self.config)
+
+    def test_setup_config_bad_method(self):
+        self.config.set('meego', 'method', 'NotRestOrXmlrpc')
+        self.assertRaises(RuntimeError,
+            self.participant.setup_config, self.config)
 
     def test_handle_lifecycle_control(self):
         ctrl = Mock()
