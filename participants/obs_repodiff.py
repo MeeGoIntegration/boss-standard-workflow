1c2f8fcbff8ebe89f59805bae19e2d059aae64f4
Reto Zingg <reto.zingg@jollamobile.com>
2012-11-01 13:06:34 +0200

    add description about the participant

--- participants/obs_repodiff.py
+++ participants/obs_repodiff.py
@@ -1,5 +1,38 @@
 #!/usr/bin/python
+"""Generates a diff of 2 obs repositories
 
+:term:`Workitem` params IN:
+
+:Parameters:
+    source(string):
+        source repository
+    target(string):
+        target repository
+
+:term:`Workitem` fields OUT
+
+:Parameters:
+    repodiff(dict):
+        Dictionary will contain fields
+          * "src_project" with the source project name
+          * "tgt_project" with the target project name
+          * "diff" = a dict containing the difference
+                  with: 'added'    = list of packages added to the
+                                     the target project
+                        'modified' = list of packages modified compared
+                                     to the target project
+                        'removed'  = list of packages removed from
+                                     source project compared to the
+                                     target project
+    msg(list):
+       List of string messages, describing the diff, can be used
+       for logging
+
+:Returns:
+    result(Boolean):
+       True if diff was found, false otherwise
+
+"""
 from boss.obs import BuildServiceParticipant
 import repo_diff
 
