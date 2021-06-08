2d4364d2ca6b01afdf3e6d159598053ff061277c
Pami Ketolainen <pami.ketolainen@jolla.com>
2021-06-08 23:47:53 +0300

    [notify_irc] Send multiple messages as one
    If the receiveing end supports multiline messages, multiple messages
    can't be sent in one connection.

--- participants/notify_irc.py
+++ participants/notify_irc.py
@@ -7,26 +7,28 @@ class ParticipantHandler(object):
     def notify(self, msg=["No 'message' for notify_irc"], channel="#meego-boss", highlight=""):
         ircbot = socket.socket()
         ircbot.connect((self.ircbot_host, self.ircbot_port))
-        for item in msg:
-            # Some bots handle multi-line messages,
-            # the old supybot Notify plugin doesn't
-            if self.split_lines:
-                mls = item.splitlines()
-            else:
-                mls = [item]
-            for m in mls:
-                m = m.strip()
-                if highlight and highlight[-1] != ' ':
-                    highlight += ' '
-                try:
-                    line = u"%s %s%s\n" % (channel, highlight, m)
-                    ircbot.send(line.encode('utf-8'))
-                except:
-                    self.log.exception("Failed to send message '%s'" % m)
-                    ircbot.send(
-                        "%s %s unable to send line, please check logs\n" %
-                        (channel, highlight)
-                    )
+        # Some bots handle multi-line messages,
+        # the old supybot Notify plugin doesn't
+        if self.split_lines:
+            parts = []
+            for m in msg:
+                parts.extend(m.split_lines())
+        else:
+            parts = ["\n\n".join(msg)]
+
+        for m in parts:
+            m = m.strip()
+            if highlight and highlight[-1] != ' ':
+                highlight += ' '
+            try:
+                line = u"%s %s%s\n" % (channel, highlight, m)
+                ircbot.send(line.encode('utf-8'))
+            except:
+                self.log.exception("Failed to send message '%s'" % m)
+                ircbot.send(
+                    "%s %s unable to send line, please check logs\n" %
+                    (channel, highlight)
+                )
         ircbot.close()
 
     def handle_wi_control(self, ctrl):
