cbd6df979654cb0cb97bb896f47423bb0c743a0c
David Greaves <david.greaves@jolla.com>
2021-06-25 16:52:49 +0100

    slipway packaging
    Signed-off-by: David Greaves <david.greaves@jolla.com>

--- modules/setup.py
+++ modules/setup.py
@@ -1,15 +1,6 @@
-#!/usr/bin/python2
-from distutils.core import setup
-import os, sys
+#!/usr/bin/env python
+import setuptools
 
-setup(name = 'BOSS modules',
-  version = '0.6.0',
-  description = 'Helper modules for BOSS participants',
-  author = 'Islam Amer',
-  author_email = 'islam.amer@jollamobile.com',
-  url =
-  'http://meego.gitorious.org/meego-infrastructure-tools/boss-standard-workflow',
-  packages = ['ots', 'boss', 'boss.bz'],
-  package_dir = {'ots' : 'ots' , 'boss' : 'boss', 'boss.bz' : 'boss/bz'},
-  py_modules = ['repo_diff']
-)
+# setup() now reads setup.cfg
+if __name__ == "__main__":
+    setuptools.setup()
